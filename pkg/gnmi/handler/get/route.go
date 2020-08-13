d91da9176db8c6bd7c60c6da61c0e492850b4f8e
sgkind <sunguanke11@mails.ucas.edu.cn>
2020-08-13 15:39:35 +0800

    在handler get中添加路由函数

---
+++ pkg/gnmi/handler/get/route.go
@@ -0,0 +1,20 @@
+package get
+
+import (
+    my_gnmi "gnmi_server/pkg/gnmi"
+)
+
+func GetServeMux() *my_gnmi.GetServeMux {
+    mux := my_gnmi.NewGetServeMux()
+    route(mux)
+    return mux
+}
+
+
+func route(mux *my_gnmi.GetServeMux) {
+    mux.AddRouter("/test", Test).
+        AddRouter("/sonic-platform/platform", ComponentInfoHandler).
+        AddRouter("/sonic-platform/platform/component-list/fan", FanInfoHandler).
+        AddRouter("/sonic-platform/platform/component-list/power-supply", PowerSupplyInfoHandler).
+        AddRouter("/sonic-platform/platform/component-list/temperature", TemperatureInfoHandler)
+}
