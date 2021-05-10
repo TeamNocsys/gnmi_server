6452978896ac30ab4603d9eeceb8d56a4827acfc
kyle <fwdssg@gmail.com>
2021-05-10 12:29:03 +0800

    默认SONIC命令执行超时时间为15秒

--- internal/pkg/utils/utils.go
+++ internal/pkg/utils/utils.go
@@ -2,11 +2,17 @@ package utils
 
 import (
     "bytes"
+    "context"
     "os/exec"
+    "time"
 )
 
 func Utils_execute_cmd(name string, arg ...string) (error, string) {
-    cmd := exec.Command(name, arg...)
+    // TODO: 后期根据不同命令，传入不同超时时间
+    ctx, cancel := context.WithTimeout(context.Background(), 15 * time.Second)
+    defer cancel()
+
+    cmd := exec.CommandContext(ctx, name, arg...)
     var buffer bytes.Buffer
     cmd.Stdout = &buffer
     err := cmd.Run()
