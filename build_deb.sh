ede31131ac26f97799dfc96635aa96904321cc12
squid_ro <squid_ro@edge-core.com>
2021-06-23 10:53:15 +0800

    Enhance for creating a debian package.
      - put commit hash info to debain control file.
      - enable gnmi service after installing process.
      - remove default username/password in .service.

--- build_deb.sh
+++ build_deb.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# put git commit hash info into debian control file
+CPATH=build/deb/DEBIAN/control
+git checkout -- $CPATH
+GIT_COMMIT=$(git describe --dirty --always)
+sed -i 's/GIT_COMMIT/'$GIT_COMMIT'/g' $CPATH
+
 GOOS=linux go build -gcflags "all=-N -l" -tags release -o build/deb/usr/local/bin cmd/gnmi/gnmi.go
 
 chmod +x build/deb/usr/local/bin/gnmi
