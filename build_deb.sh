40e3d9ba8d13cef3d90f7a211957aca8236ffae4
Charles Chou <charles_chou@edge-core.com>
2021-06-24 10:39:48 +0800

    add dynamic version by git commit id

--- build_deb.sh
+++ build_deb.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+GIT_COMMIT_SHORT=$(git rev-parse --short HEAD)
+
+sed -i "s/Version:/Version: 1.$GIT_COMMIT_SHORT/g" build/deb/DEBIAN/control
 #GOOS=linux go build -gcflags "all=-N -l" -tags release -o build/deb/usr/local/bin cmd/gnmi/gnmi.go
 dpkg -b build/deb gnmi.deb
 dpkg -c gnmi.deb
